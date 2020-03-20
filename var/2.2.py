25bfee1daace19e91aa63efe87fbb7ef6f0b560a
Simon Ejdre <9076128+Dinosurr@users.noreply.github.com>
2020-03-20 14:44:35 +0100

    added some scripts

---
+++ var/2.2.py
@@ -0,0 +1,12 @@
+num1 = int(input("Please insert the first number: "))
+num2 = int(input("Please insert the second number: "))
+
+sum = num1 + num2
+product = num1 * num2
+quota = num1 / num2
+diff = num1 - num2
+
+print("Sum =", sum)
+print("Product = %d" % product)
+print("Quota = {int_var}" .format(int_var=quota))
+print(f"Difference is = {diff}")

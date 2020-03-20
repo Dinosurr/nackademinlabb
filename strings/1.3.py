97227156a68a133bada6b10a942147591c426e48
Simon Ejdre <9076128+Dinosurr@users.noreply.github.com>
2020-03-20 14:44:54 +0100

    added some scripts

---
+++ strings/1.3.py
@@ -0,0 +1,10 @@
+inputA = input("please insert input A")
+inputB = input("please insert input B")
+
+
+new_first_stringA = "'" + inputA + "'"
+
+new_first_stringB = '""' + inputB + '""'
+result_string = new_first_stringA + '\n' + new_first_stringB
+
+print(result_string)
---
+++ strings/linebreak.py
@@ -0,0 +1,4 @@
+stringA = "'" + input("please insert input for A") + "'"
+stringB = '"' + input("please insert input for B") + '"'
+
+print(stringA + "\n" + stringB)
